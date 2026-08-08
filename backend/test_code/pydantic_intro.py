5a0c0c0d9076285d6439f567d6d45fbd80db9112
ashrafulahsan <ahsanashraful@email.com>
2026-08-08 10:27:26 +0600

    Install required package pydantic, SQLAlchemy, psycopg2, python-dotenv

---
+++ backend/test_code/pydantic_intro.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import datetime
+
+class Blog(BaseModel):
+    title: str
+    content: str
+    published: Optional[bool] = True
+    tags: List[str] = [],
+    created_at: datetime = datetime.now()
+
+first_blog = Blog(
+    title="My first blog", 
+    content="This is my first blog post", 
+    tags=["python", "fastapi"],
+    created_at=datetime.now()
+)    
+
+print(first_blog.title)
+print(first_blog.content)
+print(first_blog.created_at)
