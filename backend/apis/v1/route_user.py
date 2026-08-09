d8bfcf84dc4b870f9b46f3e2b17bc5f107502e9e
ashrafulahsan <ahsanashraful@email.com>
2026-08-09 12:59:42 +0600

    Refactor imports in route_user.py to include status for consistency

--- backend/apis/v1/route_user.py
+++ backend/apis/v1/route_user.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
-from backend.schemas.user import UserCreate, ShowUser, status
+from backend.schemas.user import UserCreate, ShowUser
 from backend.db.session import get_db
 from backend.db.repository.user import create_new_user
 
